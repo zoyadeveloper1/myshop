b75384ef9b80709962ec181f55e8c0b503499d4e
R.DurgaDevi <zoyadeveloper1@gmail.com>
2026-04-19 14:10:26 +0530

    first commit

---
+++ myshop/urls.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+
+
+
+urlpatterns = [
+
+            path('orders/', include('orders.urls', namespace='orders')),
+            path('admin/', admin.site.urls),
+            path('cart/', include('cart.urls', namespace='cart')),
+            path('', include('shop.urls', namespace='shop')),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
